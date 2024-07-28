b57d436313907360cda4cd8122bc106c4982a440
Kiril Ivanov <ivanovkirilg@gmail.com>
2024-07-28 17:45:08 +0300

    LOGR: Remove obsolete 'logfile'

--- LOGR/include/LOGR/internal.hpp
+++ LOGR/include/LOGR/internal.hpp
@@ -1,7 +1,6 @@
 #ifndef LOGR_INCLUDE_LOGR_INTERNAL
 #define LOGR_INCLUDE_LOGR_INTERNAL
 
-#include <fstream>
 #include <source_location>
 #include <sstream>
 
@@ -12,8 +11,6 @@ namespace LOGR
 namespace internal
 {
 constexpr char SEPARATOR = ';';
-
-extern thread_local std::ofstream logfile;
 
 enum class Level
 {
