0a86f341b75bad41d5cdb3260fbbc5d1f01dd04f
Kiril Ivanov <ivanovkirilg@gmail.com>
2024-11-10 11:47:02 +0200

    Rename 'parser' to 'lexer'

--- COMM/src/interface_generator/test_parser.py
+++ COMM/src/interface_generator/test_parser.py
@@ -1,14 +1,14 @@
 import unittest
 
-import parser
-from parser import *
+import interface_generator.lexer as lexer
+from interface_generator.lexer import *
 
 
-class TestParser(unittest.TestCase):
+class TestLexer(unittest.TestCase):
     def test_space_delimited_words(self):
         tu = 'hello' + '\t' + 'world' + '\n' + 'how' + ' ' + 'are' + '  ' + 'you'
 
-        tokens = parser.tokenize(tu)
+        tokens = lexer.tokenize(tu)
 
         expected = [
             WordToken('hello'),
@@ -23,7 +23,7 @@ class TestParser(unittest.TestCase):
     def test_space_delimited_punctuation(self):
         tu = '(' + '\t' + ')' + '\n' + ',' + ' ' + ';' + '  ' + ','
 
-        tokens = parser.tokenize(tu)
+        tokens = lexer.tokenize(tu)
 
         expected = [
             PunctuationToken('(', PunctuationKind.OPEN_PAREN),
@@ -39,7 +39,7 @@ class TestParser(unittest.TestCase):
         tu = 'hello!'
 
         with self.assertRaises(ValueError) as exc_context:
-            _ = parser.tokenize(tu)
+            _ = lexer.tokenize(tu)
 
         self.assertIn('punct', str(exc_context.exception))
         self.assertIn('!', str(exc_context.exception))
@@ -47,7 +47,7 @@ class TestParser(unittest.TestCase):
     def test_punctuation_delimited_words(self):
         tu = 'hello,world;how(are)you'
 
-        tokens = parser.tokenize(tu)
+        tokens = lexer.tokenize(tu)
 
         expected = [
             WordToken('hello'),
@@ -66,7 +66,7 @@ class TestParser(unittest.TestCase):
     def test_numbers(self):
         tu = '0 +123 -456 7.8 -9.0 2.2e-3 -3.3E+4 .5'
 
-        tokens = parser.tokenize(tu)
+        tokens = lexer.tokenize(tu)
 
         expected = [
             IntegerToken('0',        0),
