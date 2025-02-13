53762299140c9a4059aa61fa79b82e0cdde0c8eb
Kiril Ivanov <ivanovkirilg@gmail.com>
2025-02-13 22:23:19 +0200

    DRVR: Add trace logging

--- DRVR/src/Driver.cpp
+++ DRVR/src/Driver.cpp
@@ -1,5 +1,7 @@
 #include "Driver.hpp"
 
+#include "LOGR/Trace.hpp"
+
 #include <iostream>
 #include <thread>
 
@@ -9,7 +11,11 @@ using namespace DRVR;
 
 void Driver::accelerate(double instantaneousAcceleration)
 {
+    LOGR::Trace trace{instantaneousAcceleration};
+
     m_velocity = m_velocity + instantaneousAcceleration;
+
+    trace.log("New velocity", m_velocity);
 }
 
 double Driver::position()
@@ -24,18 +30,24 @@ double Driver::velocity()
 
 void Driver::loggingOn()
 {
+    LOGR::Trace trace{m_logCount};
+
     m_logFile << "Log " << m_logCount++ << '\n';
     m_logging = true;
 }
 
 void Driver::loggingOff()
 {
+    LOGR::Trace trace{m_logCount};
+
     m_logging = false;
     m_logFile << std::endl;
 }
 
 void Driver::run(int refreshRate_ms)
 {
+    LOGR::Trace trace{refreshRate_ms};
+
     if (m_running)
     {
         throw std::runtime_error{"already running"};
@@ -43,6 +55,8 @@ void Driver::run(int refreshRate_ms)
 
     auto job = [this, refreshRate_ms]()
     {
+        LOGR::Trace bgTrace;
+
         while (m_running)
         {
             const double deltaT = refreshRate_ms / 1000.0;
