41ccccd78b530d818bf973977a0b445a2be89576
Kiril Ivanov <ivanovkirilg@gmail.com>
2025-03-01 15:57:28 +0000

    devcontainer: Initialize clangd extension correctly

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -2,12 +2,19 @@
 
 git submodule update --recursive --init
 
+# Update/upgrade
 sudo DEBIAN_FRONTEND=noninteractive apt -y update && sudo DEBIAN_FRONTEND=noninteractive apt -y upgrade
 
-sudo DEBIAN_FRONTEND=noninteractive apt -y install lsb-release wget software-properties-common gnupg # llvm.sh dependencies
+# Install llvm.sh dependencies
+sudo DEBIAN_FRONTEND=noninteractive apt -y install lsb-release wget software-properties-common gnupg
 sudo .devcontainer/llvm.sh 21
 
+# Update/upgrade again (llvm install script adds new apt repo)
 sudo DEBIAN_FRONTEND=noninteractive apt -y update && sudo DEBIAN_FRONTEND=noninteractive apt -y upgrade
 
+# Populate build/ (make sure CMake extension doesn't try to choose Ninja)
 mkdir build ; cd build
 cmake .. && make -j
+
+# Refresh VSCode (tab) for the sake of clangd and CMake extensions
+code -r $CODESPACE_VSCODE_FOLDER
