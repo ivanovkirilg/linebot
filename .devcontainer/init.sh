eaf5a534ed0c4115a6438242c74a08d6b619d059
Kiril Ivanov <ivanovkirilg@gmail.com>
2025-03-02 10:45:51 +0000

    devcontainer: Add CMake Tools 'kit'
    Added to .vscode dir under version control, rather
    than copying it in init.sh, because the latter is
    executed parallel to extension activation, and we
    don't want CMake Tools scanning for other kits.

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -11,10 +11,3 @@ sudo .devcontainer/llvm.sh 21
 
 # Update/upgrade again (llvm install script adds new apt repo)
 sudo DEBIAN_FRONTEND=noninteractive apt -y update && sudo DEBIAN_FRONTEND=noninteractive apt -y upgrade
-
-# Populate build/ (make sure CMake extension doesn't try to choose Ninja)
-mkdir build ; cd build
-cmake .. && make -j
-
-# Refresh VSCode (tab) for the sake of clangd and CMake extensions
-code -r $CODESPACE_VSCODE_FOLDER
