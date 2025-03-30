bbb34bfa4fd561e4344dd90ac6dc80a5568ff348
Kiril Ivanov <ivanovkirilg@gmail.com>
2025-03-30 21:10:32 +0200

    COMM: Add interface lexer

---
+++ COMM/py/interface_generator/test/test_lexer.py
@@ -0,0 +1,107 @@
+import unittest
+
+from ..lexer import tokenize
+from ..tokens import *
+
+
+class TestLexer(unittest.TestCase):
+    '''
+    Keyword/punctuation/number tokens can be created via spelling directly,
+    but their kinds/values are specified to make sure they are correct.
+    '''
+    def test_space_delimited_words(self):
+        tu = 'hello' + '\t' + 'world' + '\n' + 'how' + ' ' + 'are' + '  ' + 'you'
+
+        tokens = tokenize(tu)
+
+        expected = [
+            WordToken('hello'),
+            WordToken('world'),
+            WordToken('how'),
+            WordToken('are'),
+            WordToken('you')
+        ]
+
+        self.assertEqual(tokens, expected)
+
+    def test_space_delimited_punctuation(self):
+        tu = '(' + '\t' + ')' + '\n' + ',' + ' ' + ';' + '  ' + '->'
+
+        tokens = tokenize(tu)
+
+        expected = [
+            PunctuationToken('(',  PunctuationKind.OPEN_PAREN),
+            PunctuationToken(')',  PunctuationKind.CLOSE_PAREN),
+            PunctuationToken(',',  PunctuationKind.COMMA),
+            PunctuationToken(';',  PunctuationKind.SEMICOLON),
+            PunctuationToken('->', PunctuationKind.RIGHT_ARROW),
+        ]
+
+        self.assertEqual(tokens, expected)
+
+    def test_unsupported_punctuation(self):
+        tu = 'hello!'
+
+        with self.assertRaises(ValueError) as exc_context:
+            _ = tokenize(tu)
+
+        self.assertIn('punct', str(exc_context.exception))
+        self.assertIn('!', str(exc_context.exception))
+
+    def test_punctuation_delimited_words(self):
+        tu = 'hello,world;how(are)you'
+
+        tokens = tokenize(tu)
+
+        expected = [
+            WordToken('hello'),
+            PunctuationToken(',', PunctuationKind.COMMA),
+            WordToken('world'),
+            PunctuationToken(';', PunctuationKind.SEMICOLON),
+            WordToken('how'),
+            PunctuationToken('(', PunctuationKind.OPEN_PAREN),
+            WordToken('are'),
+            PunctuationToken(')', PunctuationKind.CLOSE_PAREN),
+            WordToken('you')
+        ]
+
+        self.assertEqual(tokens, expected)
+
+    def test_numbers(self):
+        tu = '0 +123 -456 7.8 -9.0 2.2e-3 -3.3E+4 .5'
+
+        tokens = tokenize(tu)
+
+        expected = [
+            IntegerToken('0',        0),
+            IntegerToken('+123',    +123),
+            IntegerToken('-456',    -456),
+            FloatToken(  '7.8',      7.8),
+            FloatToken(  '-9.0',    -9.0),
+            FloatToken(  '2.2e-3',   2.2e-3),
+            FloatToken(  '-3.3E+4', -3.3E+4),
+            FloatToken(  '.5',        .5)
+        ]
+
+        self.assertEqual(tokens, expected)
+
+    def test_keywords_vs_words(self):
+        tu = '''method methodological METHOD
+                in inn IN
+                out outage OUT'''
+
+        tokens = tokenize(tu)
+
+        expected = [
+            KeywordToken('method', KeywordKind.METHOD),
+            WordToken('methodological'),
+            WordToken('METHOD'),
+            KeywordToken('in', KeywordKind.IN),
+            WordToken('inn'),
+            WordToken('IN'),
+            KeywordToken('out', KeywordKind.OUT),
+            WordToken('outage'),
+            WordToken('OUT')
+        ]
+
+        self.assertEqual(tokens, expected)
---
+++ COMM/py/interface_generator/tokens.py
@@ -0,0 +1,103 @@
+from enum import Enum, auto
+from dataclasses import dataclass
+
+class DataType(Enum):
+    VOID = 'void'
+
+    BYTE = 'byte'
+    BOOL = 'bool'
+
+    INT = 'int'
+    FLOAT = 'float'
+    DOUBLE = 'double'
+
+    CHAR = 'char'
+    STRING = 'string'
+
+DATA_TYPE = {
+    'void': DataType.VOID,
+    'byte': DataType.BYTE,
+    'bool': DataType.BOOL,
+    'int': DataType.INT,
+    'float': DataType.FLOAT,
+    'double': DataType.DOUBLE,
+    'char': DataType.CHAR,
+    'string': DataType.STRING,
+}
+
+class PunctuationKind(Enum):
+    OPEN_PAREN = auto()
+    CLOSE_PAREN = auto()
+    COMMA = auto()
+    SEMICOLON = auto()
+    RIGHT_ARROW = auto()
+
+PUNCTUATION = {
+    '(':  PunctuationKind.OPEN_PAREN,
+    ')':  PunctuationKind.CLOSE_PAREN,
+    ',':  PunctuationKind.COMMA,
+    ';':  PunctuationKind.SEMICOLON,
+    '->': PunctuationKind.RIGHT_ARROW,
+}
+
+class KeywordKind(Enum):
+    METHOD = auto()
+    IN = auto()
+    OUT = auto()
+
+KEYWORDS = {
+    'method': KeywordKind.METHOD,
+    'in': KeywordKind.IN,
+    'out': KeywordKind.OUT,
+}
+
+@dataclass
+class Token:
+    spelling: str
+
+@dataclass
+class WordToken(Token):
+    pass
+
+@dataclass
+class DataTypeToken(Token):
+    kind: DataType = None
+
+    def __post_init__(self):
+        if self.kind is None:
+            self.kind = DATA_TYPE[self.spelling]
+
+@dataclass
+class KeywordToken(WordToken):
+    kind: KeywordKind = None
+
+    def __post_init__(self):
+        if self.kind is None:
+            self.kind = KEYWORDS[self.spelling]
+
+@dataclass
+class PunctuationToken(Token):
+    kind: PunctuationKind = None
+
+    def __post_init__(self):
+        if self.kind is None:
+            try:
+                self.kind = PUNCTUATION[self.spelling]
+            except KeyError:
+                raise ValueError(f'Unsupported punctuation {self.spelling}')
+
+@dataclass
+class IntegerToken(Token):
+    value: int = None
+
+    def __post_init__(self):
+        if self.value is None:
+            self.value = int(self.spelling)
+
+@dataclass
+class FloatToken(Token):
+    value: float = None
+
+    def __post_init__(self):
+        if self.value is None:
+            self.value = float(self.spelling)
