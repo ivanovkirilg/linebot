4abf4463944d4d8b883425ae5e4b1ee8d99acea8
Kiril Ivanov <ivanovkirilg@gmail.com>
2025-04-07 21:38:53 +0300

    UI: Extract MoveInput into own files

---
+++ UI/inc/MoveInput.hpp
@@ -0,0 +1,69 @@
+#ifndef UI_INC_MOVE_INPUT
+#define UI_INC_MOVE_INPUT
+
+#include "UI/UserInterface.hpp"
+
+#include "DOMN/Move.hpp"
+#include "LOGR/Warning.hpp"
+
+#include <sstream>
+
+
+struct MoveInput
+{
+    DOMN::MoveType type{};
+    DOMN::Move move{};
+    const char* profilePrompt;
+
+    bool valid() const;
+
+    void tryReadMoveType();
+    void tryReadMoveProfile();
+
+    template <void (MoveInput::*method)()>
+    void retry(int retries)
+    {
+        (this->*method)();
+
+        for (int retry = 0; state == MoveInput::State::INVALID_INPUT; retry++)
+        {
+            if (retry >= retries)
+            {
+                throw UI::InvalidInputException("No valid move entered (retried)");
+            }
+
+            (this->*method)();
+        }
+    }
+
+private:
+    template<class SpecificMove, double SpecificMove::*profileCharacteristic>
+    void tryParseMoveProfile(const std::string& line)
+    {
+        SpecificMove parsed;
+        std::istringstream lineStream(line);
+        lineStream >> parsed.targetPosition >> parsed.*profileCharacteristic;
+
+        if (DOMN::isValid(parsed))
+        {
+            state = State::VALID_INPUT;
+            move.profile = parsed;
+        }
+        else
+        {
+            state = State::INVALID_INPUT;
+            LOGR::Warning("Received invalid move profile ",
+                          parsed.targetPosition,
+                          parsed.*profileCharacteristic);
+        }
+    }
+
+    enum class State
+    {
+        END_OF_INPUT,
+        INVALID_INPUT,
+        VALID_INPUT
+    } state{};
+};
+
+#endif // UI_INC_MOVE_INPUT
--- UI/src/UserInterface.cpp
+++ UI/src/UserInterface.cpp
@@ -1,13 +1,13 @@
 #include "UI/UserInterface.hpp"
 
+#include "MoveInput.hpp"
+
 #include "DOMN/Move.hpp"
 #include "LOGR/Trace.hpp"
-#include "LOGR/Warning.hpp"
 
 #include <cmath>
 #include <iostream>
 #include <optional>
-#include <sstream>
 
 using namespace UI;
 
@@ -18,123 +18,6 @@ static constexpr int NR_OF_RETRIES = 3;
 
 static constexpr size_t WIDTH = 80;
 
-static constexpr const char* LINEAR_MOVE_PROMPT     = " Enter target position & speed: ";
-static constexpr const char* TRIANGULAR_MOVE_PROMPT = " Enter target position & acceleration: ";
-
-
-struct MoveInput
-{
-    DOMN::MoveType type{};
-    DOMN::Move move{};
-    const char* profilePrompt;
-
-    bool valid() const
-    {
-        return state == State::VALID_INPUT;
-    }
-
-    void tryReadMoveType()
-    {
-        LOGR::Trace trace;
-
-        std::string line;
-        if (not std::getline(std::cin, line))
-        {
-            state = State::END_OF_INPUT;
-            return;
-        }
-
-        if (line == "l" or line == "linear")
-        {
-            state = State::VALID_INPUT;
-            move.type = DOMN::MoveType::LINEAR;
-            profilePrompt = LINEAR_MOVE_PROMPT;
-        }
-        else if (line == "t" or line == "triangular")
-        {
-            state = State::VALID_INPUT;
-            move.type = DOMN::MoveType::TRIANGULAR;
-            profilePrompt = TRIANGULAR_MOVE_PROMPT;
-        }
-        else
-        {
-            LOGR::Warning("Received invalid move type", line);
-            state = State::INVALID_INPUT;
-        }
-    }
-
-    void tryReadMoveProfile()
-    {
-        LOGR::Trace trace;
-
-        std::string line;
-        if (not std::getline(std::cin, line))
-        {
-            state = State::END_OF_INPUT;
-            return;
-        }
-
-        switch (move.type)
-        {
-            case DOMN::MoveType::LINEAR:
-                tryParseMoveProfile<DOMN::LinearMove, &DOMN::LinearMove::speed>(line);
-                break;
-
-            case DOMN::MoveType::TRIANGULAR:
-                tryParseMoveProfile<DOMN::TriangularMove, &DOMN::TriangularMove::acceleration>(line);
-                break;
-
-            default:
-                // TODO
-                break;
-        }
-    }
-
-    template <void (MoveInput::*method)()>
-    void retry(int retries)
-    {
-        (this->*method)();
-
-        for (int retry = 0; state == MoveInput::State::INVALID_INPUT; retry++)
-        {
-            if (retry >= retries)
-            {
-                throw InvalidInputException("No valid move entered (retried)");
-            }
-
-            (this->*method)();
-        }
-    }
-
-private:
-    template<class SpecificMove, double SpecificMove::*profileCharacteristic>
-    void tryParseMoveProfile(const std::string& line)
-    {
-        SpecificMove parsed;
-        std::istringstream lineStream(line);
-        lineStream >> parsed.targetPosition >> parsed.*profileCharacteristic;
-
-        if (DOMN::isValid(parsed))
-        {
-            state = State::VALID_INPUT;
-            move.profile = parsed;
-        }
-        else
-        {
-            state = State::INVALID_INPUT;
-            LOGR::Warning("Received invalid move profile ",
-                          parsed.targetPosition,
-                          parsed.*profileCharacteristic);
-        }
-    }
-
-    enum class State
-    {
-        END_OF_INPUT,
-        INVALID_INPUT,
-        VALID_INPUT
-    } state{};
-};
 
 static void draw(std::weak_ptr<const IDriver> driver, std::ostream& output)
 {
