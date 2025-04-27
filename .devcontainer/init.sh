db173832036f27a8dd47ebf3e3806f504a537942
Kiril Ivanov <ivanovkirilg@gmail.com>
2025-04-27 13:03:03 +0300

    Revert "Add cppcheck static analysis"
    This reverts commit ad10ff8288ee5ebcc29e5fd34a9c196829db5b9b.

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -5,12 +5,8 @@ git submodule update --recursive --init
 # Update/upgrade
 sudo DEBIAN_FRONTEND=noninteractive apt -y update && sudo DEBIAN_FRONTEND=noninteractive apt -y upgrade
 
-# Install cppcheck
-sudo DEBIAN_FRONTEND=noninteractive apt -y install cppcheck
-
 # Install llvm.sh dependencies
 sudo DEBIAN_FRONTEND=noninteractive apt -y install lsb-release wget software-properties-common gnupg
-# .. and clang 21
 sudo .devcontainer/llvm.sh 21
 
 # Update/upgrade again (llvm install script adds new apt repo)
