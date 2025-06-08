52acf6141503e20bbced4e10ced0bcd0553215a4
Kiril Ivanov <ivanovkirilg@gmail.com>
2025-06-08 15:24:31 +0300

    DRVR: Drop DriverProc layer, use interface directly in clients

--- DRVR/src/Driver.cpp
+++ DRVR/src/Driver.cpp
@@ -1,41 +1,75 @@
-#include "DRVR/Driver.hpp"
+#include "Driver.hpp"
+
+#include "LOGR/Trace.hpp"
+
+#include <iostream>
+#include <thread>
+
+using namespace DRVR;
+
 
 double Driver::position()
 {
-    return m_client.position();
+    return m_position;
 }
 
 double Driver::velocity()
 {
-    return m_client.velocity();
+    return m_velocity;
 }
 
 void Driver::setVelocity(double velocity)
 {
-    m_client.setVelocity(velocity);
+    LOGR::Trace trace{"current =", m_velocity, "new =", velocity};
+    m_velocity = velocity;
+    m_acceleration = 0.0;
 }
 
 void Driver::setAcceleration(double acceleration)
 {
-    m_client.setAcceleration(acceleration);
+    LOGR::Trace trace{"current =", m_acceleration, "new =", acceleration};
+    m_acceleration = acceleration;
 }
 
 void Driver::loggingOn()
 {
-    m_client.loggingOn();
+    m_logFile << "Log " << m_logCount++ << '\n';
+    m_logging = true;
 }
 
 void Driver::loggingOff()
 {
-    m_client.loggingOff();
+    m_logging = false;
+    m_logFile << std::endl;
 }
 
-void Driver::run(std::chrono::milliseconds refreshRate)
+void Driver::run(int refreshRate_ms)
 {
-    m_client.run(refreshRate.count());
+    auto job = [this, refreshRate_ms]()
+    {
+        while (m_running)
+        {
+            const double deltaT = refreshRate_ms / 1000.0;
+            m_velocity += m_acceleration * deltaT;
+            m_position += m_velocity * deltaT;
+
+            if (m_logging)
+            {
+                m_logFile << m_position << '\t';
+            }
+
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds(refreshRate_ms));
+        }
+    };
+
+    m_running = true;
+    m_background = std::thread(job);
 }
 
 void Driver::terminate()
 {
-    m_client.terminate();
+    m_running = false;
+    m_background.join();
+    m_breakLoop = true;
 }
