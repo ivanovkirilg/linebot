396ce60cf035268c003af558eae42983678eba25
Kiril Ivanov <ivanovkirilg@gmail.com>
2025-06-13 14:50:40 +0200

    fixup: interface_generator main unnecessary indent

--- COMM/py/interface_generator/__main__.py
+++ COMM/py/interface_generator/__main__.py
@@ -9,29 +9,29 @@ def main(verbose, interface_file, namespace, output_dir):
     with open(interface_file, 'r') as f:
         interface_definition = f.read()
 
-        tokens = tokenize(interface_definition)
+    tokens = tokenize(interface_definition)
 
-        declarations = parse(tokens)
-        if verbose:
-            print(declarations)
+    declarations = parse(tokens)
+    if verbose:
+        print(declarations)
 
-        generator = Generator(interface_file, namespace, declarations, output_dir)
+    generator = Generator(interface_file, namespace, declarations, output_dir)
 
-        with open(generator.get_output_path(FileKind.CLIENT, 'cpp'), 'w') as f:
-            client_cpp = generator.generate_client_cpp()
-            f.write(client_cpp)
+    with open(generator.get_output_path(FileKind.CLIENT, 'cpp'), 'w') as f:
+        client_cpp = generator.generate_client_cpp()
+        f.write(client_cpp)
 
-        with open(generator.get_output_path(FileKind.SERVER, 'cpp'), 'w') as f:
-            server_cpp = generator.generate_server_cpp()
-            f.write(server_cpp)
+    with open(generator.get_output_path(FileKind.SERVER, 'cpp'), 'w') as f:
+        server_cpp = generator.generate_server_cpp()
+        f.write(server_cpp)
 
-        with open(generator.get_output_path(FileKind.CLIENT), 'w') as f:
-            client_hpp = generator.generate_client_hpp()
-            f.write(client_hpp)
+    with open(generator.get_output_path(FileKind.CLIENT), 'w') as f:
+        client_hpp = generator.generate_client_hpp()
+        f.write(client_hpp)
 
-        with open(generator.get_output_path(FileKind.SERVER), 'w') as f:
-            server_hpp = generator.generate_server_hpp()
-            f.write(server_hpp)
+    with open(generator.get_output_path(FileKind.SERVER), 'w') as f:
+        server_hpp = generator.generate_server_hpp()
+        f.write(server_hpp)
 
 
 if __name__ == '__main__':
