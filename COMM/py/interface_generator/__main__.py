f036ca6963b00d4d3ee7789d25173f71392cc1f6
Kiril Ivanov <ivanovkirilg@gmail.com>
2025-06-14 13:16:14 +0200

    draft: add Application class in main

--- COMM/py/interface_generator/__main__.py
+++ COMM/py/interface_generator/__main__.py
@@ -4,38 +4,7 @@ from .lexer import tokenize
 from .parser import Parser
 from .generator import Generator, FileKind
 
-
-def main(verbose, interface_file, namespace, output_dir):
-    with open(interface_file, 'r') as f:
-        interface_definition = f.read()
-
-    tokens = tokenize(interface_definition, interface_file)
-
-    parser = Parser(interface_definition.splitlines())
-    declarations = parser.parse(tokens)
-    if verbose:
-        print(declarations)
-
-    generator = Generator(interface_file, namespace, declarations, output_dir)
-
-    with open(generator.get_output_path(FileKind.CLIENT, 'cpp'), 'w') as f:
-        client_cpp = generator.generate_client_cpp()
-        f.write(client_cpp)
-
-    with open(generator.get_output_path(FileKind.SERVER, 'cpp'), 'w') as f:
-        server_cpp = generator.generate_server_cpp()
-        f.write(server_cpp)
-
-    with open(generator.get_output_path(FileKind.CLIENT), 'w') as f:
-        client_hpp = generator.generate_client_hpp()
-        f.write(client_hpp)
-
-    with open(generator.get_output_path(FileKind.SERVER), 'w') as f:
-        server_hpp = generator.generate_server_hpp()
-        f.write(server_hpp)
-
-
-if __name__ == '__main__':
+def parse_arguments():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('interface_file')
 
@@ -43,6 +12,61 @@ if __name__ == '__main__':
     arg_parser.add_argument('-n', '--namespace',   type=str, required=True)
     arg_parser.add_argument('-o', '--output-dir',  type=str, required=True)
 
-    args = arg_parser.parse_args()
+    return arg_parser.parse_args()
 
-    main(args.verbose, args.interface_file, args.namespace, args.output_dir)
+class Application:
+    def __init__(self, args):
+        self._verbose        = args.verbose
+        self._interface_file = args.interface_file
+        self._namespace      = args.namespace
+        self._output_dir     = args.output_dir
+
+        with open(self._interface_file, 'r') as f:
+            self._interface_definition = f.read()
+
+    def parse(self, tokens):
+        parser = Parser(self._interface_definition.splitlines())
+
+        try:
+            declarations = parser.parse(tokens)
+        except Exception as err:
+            print(f'Failed to parse {self._interface_file}')
+            if self._verbose:
+                print(f'{err}')
+            exit(1)
+
+        return declarations
+
+    def main(self):
+        tokens = tokenize(self._interface_definition, self._interface_file)
+
+        declarations = self.parse(tokens)
+
+        if self._verbose:
+            print(declarations)
+
+        generator = Generator(self._interface_file, self._namespace, declarations, self._output_dir)
+
+        with open(generator.get_output_path(FileKind.CLIENT, 'cpp'), 'w') as f:
+            client_cpp = generator.generate_client_cpp()
+            f.write(client_cpp)
+
+        with open(generator.get_output_path(FileKind.SERVER, 'cpp'), 'w') as f:
+            server_cpp = generator.generate_server_cpp()
+            f.write(server_cpp)
+
+        with open(generator.get_output_path(FileKind.CLIENT), 'w') as f:
+            client_hpp = generator.generate_client_hpp()
+            f.write(client_hpp)
+
+        with open(generator.get_output_path(FileKind.SERVER), 'w') as f:
+            server_hpp = generator.generate_server_hpp()
+            f.write(server_hpp)
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+
+    app = Application(args)
+
+    app.main()
