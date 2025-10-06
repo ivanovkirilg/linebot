aa4b40e863011887ae697762d4b930b3e6615a59
Kiril Ivanov <ivanovkirilg@gmail.com>
2025-10-06 12:10:27 +0300

    fixup: take build type into account

--- .github/workflows/cmake-known-fails.bash
+++ .github/workflows/cmake-known-fails.bash
@@ -1,9 +1,12 @@
 #!/bin/env bash
 
+build_type="$1"
+
 function report_passed() {
     filtered_pass=$(echo $1 | sed 's/:/_/g')
-    echo "The test \"$1\" has been marked as a 'known fail', but passed!" \
-         >> "known-fail-passed-$filtered_pass-comment.txt"
+    echo "The test \"\`$1\`\" has been marked as a 'known fail', but passed" \
+         "on the \`$build_type\` build!" \
+         >> "known-fail-passed-$build_type-$filtered_pass-comment.txt"
 }
 
 known_fails=$(ctest -N -L known-fail | awk '/Test #/ { print $3 }')
@@ -12,6 +15,3 @@ for known_fail in $known_fails
 do
     ctest -R $known_fail && report_passed $known_fail
 done
-# TODO:
-# A) read the above files in a github action that posts a comment? or
-# B) post a comment directly using curl
