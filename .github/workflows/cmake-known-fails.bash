0674490c60e902301a10ced484c1b9411a326c19
Kiril Ivanov <ivanovkirilg@gmail.com>
2025-10-06 08:54:53 +0300

    draft: comment on PR when a known fail passes

---
+++ .github/workflows/cmake-known-fails.bash
@@ -0,0 +1,16 @@
+#!/bin/env bash
+
+function report_passed() {
+    echo "The test \"$1\" has been marked as a 'known fail', but passed!" \
+         >> "known-fail-passed-$1-comment.txt"
+}
+
+known_fails=$(ctest -N -L known-fail | awk '/Test #/ { print $3 }')
+
+for known_fail in $known_fails
+do
+    ctest -R $known_fail && report_passed $known_fail
+done
+# TODO:
+# A) read the above files in a github action that posts a comment? or
+# B) post a comment directly using curl
