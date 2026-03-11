396b4fcfb1bc806e647d433c7d435b4340653a94
Kiril Ivanov <ivanovkirilg@gmail.com>
2026-03-11 17:41:00 +0200

    Add plantuml to doxygen config

--- cmake/generate_docs.cmake
+++ cmake/generate_docs.cmake
@@ -3,5 +3,13 @@ if(Doxygen_FOUND)
   set(DOXYGEN_EXCLUDE_PATTERNS ${FETCHCONTENT_BASE_DIR})
   set(DOXYGEN_GENERATE_TREEVIEW YES)
 
+  file(
+    DOWNLOAD
+      https://github.com/plantuml/plantuml/releases/download/v1.2026.2/plantuml.jar
+      ${FETCHCONTENT_BASE_DIR}/plantuml.jar
+      EXPECTED_HASH SHA256=3cdce52133c424dea22425b947ae9d47f2167b0866dfcf99e714d4ea1689975c
+  )
+  set(DOXYGEN_PLANTUML_JAR_PATH ${FETCHCONTENT_BASE_DIR}/plantuml.jar)
+
   doxygen_add_docs(docs)
 endif()
